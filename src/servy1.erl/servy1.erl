b3ff98adf2893aa7a55cf4d9c39e5b25a67fe089
Roberto Nogueira <enogrob@gmail.com>
2019-08-25 14:48:40 -0300

    performing-exercises

--- src/servy1.erl/servy1.erl
+++ src/servy1.erl/servy1.erl
@@ -1,22 +1,32 @@
 -module(servy1).
--export([handle/1, parse/1]).
+% -export([handle/1, parse/1]).
+-compile(export_all).
 
-handle(Request) ->
-  parse(Request).
+  handle(Request) ->
+    format_response(
+      route(
+        parse(Request)
+      )
+    ).
 
 parse(Request) ->
-  Lines = re:split(Request, "\n"),
+  Lines = string:split(Request, "\n", all),
   FirstLine = lists:nth(2, Lines),
-  [Mehod, Path, _] = re:split(FirstLine, " "),
+  [Method, Path, _] = string:split(FirstLine, " ", all),
   #{method => Method, path => Path}.
 
-route(Request) ->
-  format_response(Request).
-format_response(_Request) ->
+route(Request = #{path := "/wildthings"}) ->
+  Request#{resp_body => "Bears, Lions, Tigers"};
+route(Request = #{path := "/bears"}) ->
+  Request#{resp_body => "Bears"};
+route(Request = #{method := Method, path := Path}) ->
+  Request#{resp_body => "No " ++ Path ++ " here!"}.
+
+format_response(#{resp_body := RespBody} = Request) ->
 "
 HTTP/1.1 200 OK
 Content-Type: text/html
-Content-Length: 20
+Content-Length: " ++ integer_to_list(string:length(RespBody)) ++ "
 
-Bears, Lions, Tigers
+" ++ RespBody ++ "
 ".
--- src/servy1.erl/servy1_tests.erl
+++ src/servy1.erl/servy1_tests.erl
@@ -1,12 +1,12 @@
 % To run tests:
 % erl -make
-% erl -noshell -eval "eunit:test(hello_world, [verbose])" -s init stop
+% erl -noshell -eval "eunit:test(servy1_tests, [verbose])" -s init stop
 %
 
 -module(servy1_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-no_name_test() ->
+wildthings_test() ->
 Request = "
 GET /wildthings HTTP/1.1
 Host: example.com
@@ -20,5 +20,39 @@ Content-Type: text/html
 Content-Length: 20
 
 Bears, Lions, Tigers
+",
+?assertEqual(Result, servy1:handle(Request)).
+
+bears_test() ->
+Request = "
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+",
+Result = "
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 5
+
+Bears
+",
+?assertEqual(Result, servy1:handle(Request)).
+
+no_path_test() ->
+Request = "
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+",
+Result = "
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 18
+
+No /wildlife here!
 ",
 ?assertEqual(Result, servy1:handle(Request)).
